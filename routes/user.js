261ce00643ed5addb65cac1c3dc3dee706e0b5fa
muntasir-fahim <fa3877957@gmail.com>
2023-06-13 22:44:33 +0600

    device api added

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,15 @@
 const express = require("express");
 
-const { signupUser, loginUser } = require("../controllers/user");
+const {
+  signupUser,
+  loginUser,
+  getUser,
+  getUsers,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user");
+const authMiddleware = require("../middlewares/auth.middleware");
+const isAdmin = require("../middlewares/admin.middleware");
 
 //router
 const router = express.Router();
@@ -8,7 +17,19 @@ const router = express.Router();
 // login route
 router.post("/login", loginUser);
 
-// signup user
+// signup route
 router.post("/signup", signupUser);
+
+// // get all users
+router.get("/all", authMiddleware, isAdmin, getUsers);
+
+// get an user
+router.get("/:userId", authMiddleware, getUser);
+
+// // update an user
+router.patch("/:userId", authMiddleware, updateUser);
+
+// // delete an user
+router.delete("/:userId", authMiddleware, deleteUser);
 
 module.exports = router;
